4be5b271d97fb7b6807351cd0daad1285c9ae634
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-17 20:37:00 +0200

    Completed service layer by implementing common functionality on a base service implementation and by injecting newly created repositories.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/service/BaseServiceImpl.java
@@ -0,0 +1,47 @@
+package gr.codelearn.spring.showcase.app.service;
+
+import gr.codelearn.spring.showcase.app.domain.BaseModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class BaseServiceImpl<T extends BaseModel> implements BaseService<T, Long> {
+	public abstract JpaRepository<T, Long> getRepository();
+
+	public T create(T item) {
+		return getRepository().save(item);
+	}
+
+	public List<T> createAll(List<T> items) {
+		return getRepository().saveAll(items);
+	}
+
+	public List<T> createAll(T... items) {
+		return createAll(Arrays.asList(items));
+	}
+
+	public void update(T item) {
+		getRepository().save(item);
+	}
+
+	public void delete(T item) {
+		getRepository().delete(item);
+	}
+
+	public void deleteById(Long id) {
+		getRepository().deleteById(id);
+	}
+
+	public T get(Long id) {
+		return getRepository().getById(id);
+	}
+
+	public boolean exists(T item) {
+		return getRepository().existsById(item.getId());
+	}
+
+	public List<T> findAll() {
+		return getRepository().findAll();
+	}
+}
