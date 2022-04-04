a0ede244800924bf768dba30f95108640a80acbf
IoannisKlian <giannisklian@gmail.com>
2022-04-04 19:07:41 +0300

    Create controller per domain model and an abstract controller with common functionality per domain model

---
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package gr.codelearn.spring.showcase.app.controller;
+
+import gr.codelearn.spring.showcase.app.domain.Customer;
+import gr.codelearn.spring.showcase.app.service.BaseService;
+import gr.codelearn.spring.showcase.app.service.CustomerService;
+import gr.codelearn.spring.showcase.app.transfer.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customers")
+public class CustomerController extends AbstractController<Customer> {
+	private final CustomerService customerService;
+
+	@Override
+	protected BaseService<Customer, Long> getBaseService() {
+		return customerService;
+	}
+
+	@GetMapping(params = {"email"})
+	public ResponseEntity<ApiResponse<Customer>> findByEmail(@RequestParam String email) {
+		return new ResponseEntity<>(ApiResponse.<Customer>builder().data(customerService.findByEmail(email)).build(),
+									HttpStatus.OK);
+	}
+}
---
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/controller/OrderController.java
@@ -0,0 +1,31 @@
+package gr.codelearn.spring.showcase.app.controller;
+
+import gr.codelearn.spring.showcase.app.domain.Order;
+import gr.codelearn.spring.showcase.app.service.BaseService;
+import gr.codelearn.spring.showcase.app.service.OrderService;
+import gr.codelearn.spring.showcase.app.transfer.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController extends AbstractController<Order>{
+	private final OrderService orderService;
+	@Override
+	protected BaseService<Order, Long> getBaseService() {
+		return orderService;
+	}
+	@GetMapping
+	@Override
+	public ResponseEntity<ApiResponse<List<Order>>> findAll() {
+		return new ResponseEntity<>(ApiResponse.<List<Order>>builder().data(orderService.findAllLazy()).build(),
+									HttpStatus.OK);
+	}
+}
