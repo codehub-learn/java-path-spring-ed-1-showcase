2fc12875c12d78d556ef3350c8cc5d181a0c5d68
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-04-12 19:50:11 +0300

    Introduced interface for JokeServiceImpl service.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/service/demo/JokeServiceImpl.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/service/demo/JokeServiceImpl.java
@@ -6,28 +6,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 @RequiredArgsConstructor
-public class JokeServiceImpl extends BaseComponent {
+public class JokeServiceImpl extends BaseComponent implements JokeService {
 	private final RestTemplate restTemplate;
-
 	private final String URL_TEMPLATE = "https://v2.jokeapi.dev/joke/";
 
+	@Override
 	public String get(String[] categories, String... blacklistFlags) {
-		String url = prepareURL(categories, blacklistFlags);
+		String url = prepareUrl(categories, blacklistFlags);
 		ResponseEntity<JsonNode> response = restTemplate.getForEntity(url, JsonNode.class);
 		JsonNode body = response.getBody();
 		return body.get("joke").asText();
 	}
 
-	private String prepareURL(String[] categories, String... blacklistFlags) {
-		UriComponents builtURI = UriComponentsBuilder.fromUriString(URL_TEMPLATE).pathSegment(
-				String.join(",", categories)).queryParam("type", "single")
-				.queryParam("blacklistFlags", String.join(",", blacklistFlags))
-													 .build();
-		return builtURI.toString();
+	private String prepareUrl(String[] categories, String... blacklistFlags) {
+		//@formatter:off
+		return
+				UriComponentsBuilder
+						.fromUriString(URL_TEMPLATE)
+						.pathSegment(String.join(",", categories))
+						.queryParam("blacklistFlags", String.join(",", blacklistFlags))
+						.queryParam("type", "single")
+						.build()
+						.toString();
+		//@formatter:on
 	}
+
 }
