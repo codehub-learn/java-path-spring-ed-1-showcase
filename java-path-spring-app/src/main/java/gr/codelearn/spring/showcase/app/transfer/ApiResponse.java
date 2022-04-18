4b509f93b7bb5fab450bfd60a187118dfb091b6b
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-04-18 20:48:38 +0300

    Spring async method execution implementation showcase.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

--- java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/transfer/ApiResponse.java
+++ java-path-spring-app/src/main/java/gr/codelearn/spring/showcase/app/transfer/ApiResponse.java
@@ -1,19 +1,23 @@
 package gr.codelearn.spring.showcase.app.transfer;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Locale;
 import java.util.UUID;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ApiResponse<T> implements Serializable {
 	String transactionId = UUID.randomUUID().toString().toUpperCase(Locale.ROOT);
-	@JsonFormat(shape=JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd hh:mm:ss.SS")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss.SS")
 	LocalDateTime createdAt = LocalDateTime.now();
 	T data;
 	ApiError apiError;
